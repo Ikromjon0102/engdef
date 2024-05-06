5edec7bc15883e2ad5e1f6ead9b75097c7f07620
ikromjon007 <ikromjon7367@gmail.com>
2024-05-06 18:34:08 +0500

    psqlda db qo'shildi

--- data/config.py
+++ data/config.py
@@ -9,6 +9,14 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
 
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASS")
+DB_NAME = env.str("DB_NAME")
+DB_HOST = env.str("DB_HOST")
+DB_PORT = env.str("DB_PORT")
+
+
+
 
 # import os
 # BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
